aef978a2fb0cc8ce31b37fccb03a69ffc57a190e
kwan <kanldark135@gmail.com>
2023-06-15 18:40:26 +0900

    v_230615_modified option price data pickling

--- option_price_sort_monthlies.py
+++ option_price_sort_monthlies.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Feb  2 14:07:21 2023
-
 @author: Kwan
 """
 
@@ -11,60 +9,55 @@ import pandas as pd
 import numpy as np
 from datetime import datetime as dt
 
-df = pd.read_excel("C:/Users/문희관/Desktop/weeklies.xlsx", sheet_name = None, skiprows = 7, index_col = 0)
 
+pre = pd.read_pickle("C:/Users/문희관/Desktop/option/data_pickle/monthly.pkl")
 
-def sort_loop(df):
+df = pd.read_excel("C:/Users/문희관/Desktop/옵션데이터/2023.xlsx", skiprows = 7, index_col = 0)
 
-    df.columns = df.loc['Name']
-    df.drop(df.index[0:5], axis = 0, inplace = True)
-    
-    # columns to multiindexed columns
-    
-    dummy = df.columns.drop_duplicates()
-    
-    callput = []
-    exp = df.iat[1, 2]
-    exp = dt.strptime(str(exp), "%Y%m%d")
-    strike = []
-    
-    for i in dummy:
-        
-        ii = i.split(" ")
-        
-        callput.append(ii[1])
-        strike.append(ii[3])
-    
-    callput = np.unique(callput)
-    exp2 = [exp]
-    strike = np.unique(strike)
-    title = df.loc['D A T E'].unique()
-    
-    ind = pd.MultiIndex.from_product([callput, exp2, strike, title], names = ['cp' , 'expiry', 'strike', 'title'])
-    
-    df.drop(index = ['D A T E'], axis = 0, inplace = True)
-    df.index.name = None
-    
-    df2 = df.copy()
-    
-    df2.columns = ind
-    df2.drop("만기일", axis = 1, level = 3, inplace = True)
-    df2 = df2.loc[df2.index <= exp]
-    
-    df_result = df2.melt(ignore_index = False)
+# 불필요한 데이터 정리
+df.columns = df.loc['Name']
+df.index.name = 'date'
+df.drop(df.index[0:5], axis = 0, inplace = True)
 
-    return df_result
+# 종목 - 만기매칭 Series 따로 생성
+bool_idx = df.loc['D A T E'].isin(['만기일'])
+df_exp = df.iloc[3].loc[bool_idx].apply(lambda x : dt.strptime(str(x), "%Y%m%d"))
 
+# callput / expiry / strike / 속성값으로 구성된 multiindex 생성
+callput = list()
+expiry = list()
+strike = list()
 
-dummy_frame = pd.DataFrame()
+for i in df.columns:
+    expiry.append(df_exp[i])
+    ii = i.split(" ")
+    callput.append(str(ii[1]))
+    strike.append(float(ii[3]))
 
-for keys in df.keys():
-            
-        result = sort_loop(df[keys])
-        
-        dummy_frame = pd.concat([dummy_frame, result], axis = 0)
-    
+title = df.loc['D A T E'] # 종가 / 내재변동성 두개로 구성된 값
+expiry = pd.to_datetime(expiry)
 
+ind = pd.MultiIndex.from_arrays([callput, expiry, strike, title], names = ['cp', 'expiry', 'strike','title'])
 
+df2 = df.copy()
+df2.columns = ind
 
+idx = pd.IndexSlice
+df2 = df2.loc[:, idx[:, :, :, ~bool_idx]] # 만기일 column 은 전부 제거
+df2.drop(index = ['D A T E'], axis = 0, inplace = True) # D A T E 행도 전부 제거
+df2.index = pd.to_datetime(df2.index)
+
+df_result = df2.melt(ignore_index = False)
+df_result = df_result.assign(dte = (df_result.expiry - df_result.index) / pd.Timedelta(1, "D") + 1)
+df_result = df_result.loc[df_result['dte'] >= 1]
+
+# 0제거
+post = df_result[df_result.value != 0]
+
+# 둘이 합치기
+final = pd.concat([pre, post], axis = 0)
+final = final.drop_duplicates()
+
+# pickle 파일로 다시 빼기
+final.to_pickle("C:/Users/문희관/Desktop/option/data_pickle/monthly.pkl")
 
